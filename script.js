772147aec178122a0089821bed366560d01a35bf
Miguel Fiais <miguel.fiais10@gmail.com>
2022-07-20 02:04:56 -0300

    adicionado brim e viper

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const buttonOmen = document.getElementById("omen")
 const buttonBrim = document.getElementById("brim")
 const buttonViper = document.getElementById("viper")
 const buttonSova = document.getElementById("sova")
+const hab1 = document.getElementById("hab-1")
+const hab2 = document.getElementById("hab-2")
+const hab3 = document.getElementById("hab-3")
+const hab4 = document.getElementById("hab-4")
+const back = document.getElementById("back")
 
 const addJett = () =>{
     const nameAgent = document.getElementById("name-agent")
     const funcAgent = document.getElementById("func-agent")
     const imgAgent = document.getElementById("img-agent")
-    const hab1 = document.getElementById("hab-1")
-    const hab2 = document.getElementById("hab-2")
-    const hab3 = document.getElementById("hab-3")
-    const hab4 = document.getElementById("hab-4")
+    back.style.backgroundColor = "rgba(3,105,255,0.5) ";
     nameAgent.innerHTML = "Jett"
     funcAgent.innerHTML = "Duelista"
     imgAgent.src = "./img/jett.png"
@@ -25,27 +27,45 @@ const addOmen = () =>{
     const nameAgent = document.getElementById("name-agent")
     const funcAgent = document.getElementById("func-agent")
     const imgAgent = document.getElementById("img-agent")
+    back.style.backgroundColor = "rgba(3,105,255,0.5) ";
     nameAgent.innerHTML = "Omen"
     funcAgent.innerHTML = "Controlador"
     imgAgent.src = "./img/omen.png"
+    hab1.innerHTML = "<b>Q</b> – Paranoia"
+    hab2.innerHTML = "<b>E</b> – Manto Sombrio"
+    hab3.innerHTML = "<b>C</b> – Passos Tenebrosos"
+    hab4.innerHTML = "<b>X</b> – Salto das Sombras"
+    
     
 }
 const addBrim = () =>{
     const nameAgent = document.getElementById("name-agent")
     const funcAgent = document.getElementById("func-agent")
     const imgAgent = document.getElementById("img-agent")
+    back.style.backgroundColor = "rgba(251,153,82,0.5) ";
     nameAgent.innerHTML = "Brimstone"
     funcAgent.innerHTML = "Controlador"
-    imgAgent.src = "./img/omen.png"
+    imgAgent.src = "./img/brim.png"
+    hab1.innerHTML = "<b>Q</b> – Incendiário"
+    hab2.innerHTML = "<b>E</b> – Fumaça Celeste"
+    hab3.innerHTML = "<b>C</b> – Sinalizador Estimulante"
+    hab4.innerHTML = "<b>X</b> – Ataque Orbital"
+
     
 }
 const addViper = () =>{
     const nameAgent = document.getElementById("name-agent")
     const funcAgent = document.getElementById("func-agent")
     const imgAgent = document.getElementById("img-agent")
+    back.style.backgroundColor = "rgba(0,97,3,0.5)";
     nameAgent.innerHTML = "Viper"
     funcAgent.innerHTML = "Controlador"
-    imgAgent.src = "./img/omen.png"
+    imgAgent.src = "./img/viper2.png"
+    hab1.innerHTML = "<b>Q</b> – Nuvem Venenosa"
+    hab2.innerHTML = "<b>E</b> – Cortina Tóxica"
+    hab3.innerHTML = "<b>C</b> – Veneno de Cobra"
+    hab4.innerHTML = "<b>X</b> – Poço Peçonhento"
+    
     
 }
 
